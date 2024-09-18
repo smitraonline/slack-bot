3fa46dbbb4142c76e2b4b58ca2a2b2c66d9b5337
Subhajit Mitra <subhajit.mitra@in.ibm.com>
2024-09-18 10:34:25 +0530

    Update code

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const { App } = require('@slack/bolt');
+const axios = require('axios')
 
 const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
 });
+
+//http://169.53.229.26:8000/docs
+
+// curl - X 'POST' \
+// 'http://169.53.229.26:8000/tasks/task/usecase-classification-plugin' \
+// -H 'accept: application/json' \
+// -H 'Content-Type: application/json' \
+// -d '{
+// "input": "I need access to Saviynt"
+// }'
+
+var generic = [];
+generic.push({ text: 'Following are request details, ', response_type: 'text' })
+
+const processMessage = async (message) => {
+    //call skillet
+    data = { "input": message }
+    config = {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+    response = await axios.post("http://169.53.229.26:8000/tasks/task/usecase-classification-plugin", data, config)
+    console.log("--response--")
+    console.log(response)
+    return response
+}
+
 
 /* Add functionality here */
 app.event('app_home_opened', ({ event, say }) => {
@@ -13,8 +43,10 @@ app.event('app_home_opened', ({ event, say }) => {
 app.message(async ({ message, say }) => {
     // Filter out message events with subtypes (see https://api.slack.com/events/message)
     if (message.subtype === undefined || message.subtype === 'bot_message') {
-        const reversedText = [...message.text].reverse().join("");
-        await say(reversedText);
+        //const reversedText = [...message.text].reverse().join("_");
+        console.log(message)
+        response = await processMessage(message.text)
+        await say({ text: response.data.output });
     }
 });
 
